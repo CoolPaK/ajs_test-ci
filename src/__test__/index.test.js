aabb3d4ba6e3ce9175cc647527e1f2b9ec125ac1
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-09-20 13:55:29 +0300

    add file

---
+++ src/__test__/index.test.js
@@ -0,0 +1,13 @@
+const getHealthStatus = require('../index');
+
+test('returns "healthy" when health is greater than 50', () => {
+    expect(getHealthStatus({ name: 'Маг', health: 90 })).toBe('healthy');
+});
+
+test('returns "wounded" when health is between 15 and 50', () => {
+    expect(getHealthStatus({ name: 'Маг', health: 30 })).toBe('wounded');
+});
+
+test('returns "critical" when health is less than 15', () => {
+    expect(getHealthStatus({ name: 'Маг', health: 10 })).toBe('critical');
+});
---
+++ src/__test__/sort.test.js
@@ -0,0 +1,24 @@
+const sortHeroesByHealth = require('../sort');
+
+test('should sort heroes by health in descending order', () => {
+    const heroes = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+    ];
+
+    const sortedHeroes = sortHeroesByHealth(heroes);
+
+    expect(sortedHeroes).toEqual([
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+    ]);
+
+    
+    expect(sortedHeroes).not.toBe([
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+    ]);
+});
